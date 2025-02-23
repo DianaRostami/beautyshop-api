d4acdb3b2b75e290a46ab3e8ac70734c50873aa4
DianaRostami <diana84rostami@gmail.com>
2025-02-23 16:37:28 +0330

    apply search filters

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Q, F
+from decimal import Decimal
 
 
 class Category(models.Model):
@@ -11,12 +13,26 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
+    discount = models.PositiveIntegerField(default=0)
+
+    objects = models.Manager()  # Use custom manager
 
     def __str__(self):
         return self.name
+
+    def final_price(self):
+        if self.discount != 0:
+            return self.price - (self.price * Decimal(self.discount) / 100)
+        return self.price
+
+    #TODO: study this func
+    def decrease_stock(self, quantity):
+        if self.stock >= quantity:
+            Product.objects.filter(id=self.id, stock__gte=quantity).update(stock=F('stock') - quantity)
 
 
 class Order(models.Model):
@@ -33,8 +49,21 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"order {self.id} - {self.customer_name}"
+        return f"order {self.id} - {self.customer_name} - {self.status}"
 
 
 class User(models.Model):
     pass
+
+
+# for search better
+class ProductManager(models.Manager):
+    def search_products(self, query):
+        return self.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(price__icontains=query)
+        )
+
+    def available_or_discounted(self):
+        return self.filter(Q(is_available=True) | Q(discount__gt=0))
